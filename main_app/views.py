46505817512fa5787c6013d0ed0684b2c6cedd5f
Yi Chien Lee <yician.lee@gmail.com>
2024-04-22 14:54:49 -0700

    wip: add detail page for each finch

--- main_app/views.py
+++ main_app/views.py
@@ -16,3 +16,10 @@ def finches_index(request):
   return render(request, 'finches/index.html', {
     'finches': finches
   })
+  
+def finches_detail(request, finch_id):
+    # we need to communicate with the database here
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', {
+        'finch': finch
+    })
