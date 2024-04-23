44b796a39c642a55c3ff17273cfacb558ce1dcbe
Yi Chien Lee <yician.lee@gmail.com>
2024-04-23 15:05:10 -0700

    finish part 4

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 class Finch(models.Model):
@@ -10,3 +12,30 @@ class Finch(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"finch_id": self.id})
+
+    def fed_for_today(self):
+        MEALS = (
+            ('B', 'Breakfast'),
+            ('L', 'Lunch'),
+            ('D', 'Dinner')
+        )
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+class Feeding(models.Model):
+    MEALS = (
+        ('B', 'Breakfast'),
+        ('L', 'Lunch'),
+        ('D', 'Dinner')
+    )
+    date = models.DateField('feeding date')
+    meal = models.CharField(max_length=1, choices=MEALS, default='B')
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    class Meta:
+        ordering = ('-date',)
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 
 # Define the home view
 def home(request):
@@ -20,6 +22,29 @@ def finches_index(request):
 def finches_detail(request, finch_id):
     # we need to communicate with the database here
     finch = Finch.objects.get(id=finch_id)
+    feeding_form = FeedingForm()
     return render(request, 'finches/detail.html', {
-        'finch': finch
+        'finch': finch,
+        'feeding_form': feeding_form
     })
+    
+def add_feeding(request, finch_id):
+  submitted_form = FeedingForm(request.POST)
+  if submitted_form.is_valid:
+    new_feeding = submitted_form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+    
+  return redirect('detail', finch_id=finch_id)
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+  
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ('color', 'description', 'habitat', 'region')
+  
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
